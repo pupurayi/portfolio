c8fa0de5cf13de4d961827dae3c6e0be550c6d7d
Pupurayi <pupurayi@users.noreply.github.com>
2020-08-15 14:48:50 +0200

    typo

--- src/components/Study.vue
+++ src/components/Study.vue
@@ -5,15 +5,15 @@
     Advanced Level ,3 passes.
   </p>
 
-  <p> Here is a little story about me, am always proud to share  for it ministers how much things do not always go our way but still tends to be the best thing that ever happened.<br>
+  <p> Here is a little story about me, am always proud to share  for it tells how much things do not always go our way but still tends to be the best thing that ever happened.<br>
     During Advanced levels, I worked hard enough to come out with not so  many points.
     At this stage i was kinda heartbrocken for i wanted to do Pharmacy. I applied here at HIT and i still remember, Software Engineering
     was my last prefference. I was actually scared when i got the place,my sister there told me how much being a HIT student was very hard
-   and on top of it i had no history with computers. Iwas scared kani &#128550;,lucky enough,here i am...I can actually see that this was the best
-   decision i ever made. It reminds me of how God knows what is best for us. I fell in love with SE&#128525; the first lecture when we were told how much we can do and how people like us are vital in todays living. The fact tha we will be solving problems
+   and on top of it i had no history with computers. Iwas scared hey &#128550;,lucky enough,here i am...I can actually see that this was the best
+   decision i ever made. It reminds me of how God knows what is best for us. I fell in love with SE &#128525; the first lecture when we were told how much we can do and how people like us are vital in todays living. The fact tha we will be solving problems
    in a technical way was the most fascinating. I imagined myself putting a smile on ones face after having made a system for him.
-   one of the most motivating factors came from my lecturer - my favourite if i can say. I actually saw myself through him. He wanted to do Pharmacyas well but he was offered a place to study Computer Science instead.
-   Fastforward , he has made a lot of functioning systems some for the country. It gives the vibe that I too can make it. The vibe that nothing is impossible.
+   one of the most motivating factors came from my lecturer - my favourite if i can say. I actually saw myself through him. He wanted to do Pharmacy as well but he was offered a place to study Computer Science instead.
+   Fastforward, he has made a lot of functioning systems some for the country. It gives the vibe that I too can make it. The vibe that nothing is impossible.
  </p>
 </template>
 
