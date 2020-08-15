8922b2d5d5abc2bfcd3d986b0024d6f23bbef5eb
Pupurayi <pupurayi@users.noreply.github.com>
2020-08-15 09:08:13 +0200

    removed a folder

--- src/components/Inspiration.vue
+++ src/components/Inspiration.vue
@@ -4,7 +4,7 @@
    My inspiration comes from those who have made it and im confident that one day I will testify. These are the quotes that keep me going, whenever i feel i cant make it
    i always get back to them.<br>
    
-   <q>There is nothing impossible to him who try.</q><i>Alexander the Great</i>
+   <q>There is nothing impossible to him who try.</q><i>Alexander the Great</i><br>
     <q>The journey of a thousand miles begins with one step.</q><i>Lao Tzu</i><br>
      <q>If my mind can conceive it,and my heart can believe it - then i can achieve it.</q><i>Muhammed Ali</i><br>
     His grace is always sufficient!
